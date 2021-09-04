735bcec2c5b98bae7847d1aeed9f36e959871615
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-09-04 09:18:15 +0900

    simple p5js script

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let prevX;
+let prevY;
+
 function setup() {
   createCanvas(400, 400);
+  prevX = mouseX;
+  prevY = mouseY;
 }
 
 function draw() {
-  background(220);
+  background(32);
+  fill(200);
+  noStroke();
+  let d = dist(prevX, prevY, mouseX, mouseY);
+  circle(mouseX, mouseY, d);
+  prevX = mouseX;
+  prevY = mouseY;
 }
